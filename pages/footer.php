7089e0a05296bac79ce03f094e94644d99d9c0ce
Alireza Ebrahimi <ali6648e@gmail.com>
2026-08-30 12:28:47 +0330

    Lock down the config/ directory entirely from the web
    Was denying only .php/.sql/.env/.ini/.log by extension, so a stray file
    like config.php.bak-... would still be downloadable. Deny all access to
    the directory instead; nothing in it is meant to be fetched over HTTP.

--- pages/footer.php
+++ pages/footer.php
@@ -54,7 +54,7 @@ require_once '../includes/version.php';
     <span>در شرکت</span>
     <span class="company">آوای شرق ملک</span>
         <span> | </span>
-        <span class="footer-version" title="آخرین به‌روزرسانی: ۱۴۰۵/۰۶/۰۸ - ۱۱:۳۵">
-            نسخه: ۶.۰۶
+        <span class="footer-version" title="آخرین به‌روزرسانی: ۱۴۰۵/۰۶/۰۸ - ۱۲:۲۳">
+            نسخه: ۶.۰۷
         </span>
 </div>
